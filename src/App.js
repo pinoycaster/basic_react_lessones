b84cd2c2f07e215ea26bab24a39213d7460690f2
Spencer Castillo <spencercastillo@Spencers-MacBook-Air.local>
2023-07-20 13:27:57 +0800

    react_navigation_finished

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import AboutMe from './AboutMe';
 import Homepage from './Homepage';
+import {Routes, Route} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <nav className='nav'>
-        <a href='#' className='nav-item'>Homepage </a>
-        <a href='#' className='nav-item'>About Me </a>
+        <Link to="/" className="nav-item">Homepage</Link>
+        <Link to="/about-me" className="nav-item">About Me</Link>
       </nav>
 
-      <Homepage />
-      <AboutMe />
+      <Routes>
+      <Route path='/' element={<Homepage />} />
+      <Route path='/about-me' element={<AboutMe />} />
+      </Routes>
 
     </div>
 
