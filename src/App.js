13dd3c5b4a19361bb02a0dda62997b93a856d70e
Spencer Castillo <spencercastillo@Spencers-MacBook-Air.local>
2023-07-25 20:15:14 +0800

    calculator_react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
-import AboutMe from './AboutMe';
-import Homepage from './Homepage';
-import {Routes, Route} from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import {
+  useState,
+  useRef,
+  useEffect
+} from "react"; 
 
-function App() {
-  return (
-    <div className="App">
-      <nav className='nav'>
-        <Link to="/" className="nav-item">Homepage</Link>
-        <Link to="/about-me" className="nav-item">About Me</Link>
-      </nav>
+import "./App.css";
 
-      <Routes>
-      <Route path='/' element={<Homepage />} />
-      <Route path='/about-me' element={<AboutMe />} />
-      </Routes>
+function App() { 
+  const inputRef = useRef(null); 
+  const resultRef = useRef(null); 
+  const [result, setResult] = useState(0); 
 
-    </div>
+  useEffect(() => {
+    inputRef.current.value = "";
+  }, [result]);
+ 
+  function plus(e) { 
+    e.preventDefault(); 
+    const inputValue = Number(inputRef.current.value);
+    setResult((currentResult) => currentResult + inputValue);
+    inputRef.current.value = "";
+};
 
-  );
-}
+ 
+  function minus(e) { 
+  	// Add the code for the minus function 
+    e.preventDefault(); 
+    const inputValue = Number(inputRef.current.value);
+    setResult((currentResult) => {
+      if(currentResult === 0){
+        return inputValue
+      } 
+      return currentResult - inputValue;
+  });
 
-export default App;
+    inputRef.current.value = "";
+  };
+ 
+  function times(e) { 
+    // Add the code for the plus function 
+    e.preventDefault(); 
+    const inputValue = Number(inputRef.current.value);
+    setResult((currentResult) => {
+      if(currentResult === 0){
+        return inputValue
+      } 
+      return currentResult * inputValue;
+  });
+
+    inputRef.current.value = "";
+  }; 
+ 
+  function divide(e) { 
+    // Add the code for the divide function 
+    e.preventDefault(); 
+    const inputValue = Number(inputRef.current.value);
+    setResult((currentResult) => {
+      if(currentResult === 0){
+        return inputValue
+      } 
+      return currentResult / inputValue;
+  });
+
+    inputRef.current.value = "";
+
+
+  };
+ 
+  function resetInput(e) { 
+    // Add the code for the resetInput function 
+    e.preventDefault();
+    inputRef.current.value = "";
+
+  }; 
+ 
+  function resetResult(e) { 
+  	// Add the code for the resetResult function 
+    e.preventDefault();
+    setResult(0);
+  }; 
+ 
+  return ( 
+    <div className="App"> 
+      <div> 
+        <h1>Simplest Working Calculator</h1> 
+      </div> 
+      <form> 
+        <p ref={resultRef}>{result} </p> 
+        <input
+          pattern="[0-9]" 
+          ref={inputRef} 
+          type="number" 
+          placeholder="Type a number" 
+        /> 
+        <button onClick={plus}>add</button>
+        <button onClick={minus}>subtract</button>
+        <button onClick={times}>multiply</button>
+        <button onClick={divide}>divide</button>
+        
+        <button onClick={resetInput}>Reset input</button>
+        <button onClick={resetResult}>Reset Result</button>
+      </form> 
+    </div> 
+  ); 
+} 
+ 
+export default App; 
